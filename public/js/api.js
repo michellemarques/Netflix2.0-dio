3a2310ccd98338afdcc01d304358479b5f27f082
Michelle Marques <michellea.m.silva@gmail.com>
2022-12-27 15:20:32 -0300

    Página inicial da Netflix

---
+++ public/js/api.js
@@ -0,0 +1,4 @@
+let api_key = "1423bce51d7d1544953a0861ab19e850"
+let  img_url = "https://image.tmdb.org/t/p/w500"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+let movies_genres_http = "https://api.themoviedb.org/3/discover/movie?"
